cee55b618dd05f9837b4dc683b35adc3673b3d3e
liutianyu189 <74762431+liutianyu189@users.noreply.github.com>
2022-01-07 15:06:17 +0800

    Add files via upload

---
+++ 超分重建/predict.py
@@ -0,0 +1,42 @@
+import torch
+from PIL import Image
+from model import fsrcnn
+import numpy as np
+from torchvision import transforms
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")#使用GPU设置
+trans=transforms.ToTensor()
+img = Image.open('test.jpg')
+img=trans(img)
+img = torch.unsqueeze(img,0)
+model = fsrcnn(upscale_factor=2).to(device)
+model.load_state_dict(torch.load('logs/13.64638.pkl', map_location=device))#载入模型参数
+model.eval()
+with torch.no_grad():
+    out = model(img.to(device)).cpu()
+    out=torch.squeeze(out)
+
+    out=(out*255).numpy()
+    out = out.transpose([1, 2, 0])
+    out = np.clip(out, 0, 255)
+    out = np.uint8(out)
+    out=Image.fromarray(out)
+    out.save('out.jpg')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 超分重建/train.py
@@ -0,0 +1,44 @@
+from model import fsrcnn
+from math import log10
+import torch.nn as nn
+import torch.optim as optim
+import torch
+from data_read import get_dataloader
+from tqdm import tqdm
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+batchsize=32
+train_loader, valid_loader = get_dataloader(batchsize, 'train_imgLR', 'train_imgHR','test_imgLR','test_imgHR')
+model = fsrcnn(upscale_factor=2).to(device)
+criterion = nn.L1Loss()
+optimizer = optim.Adam(model.parameters(), lr = 0.0005)
+epochs=150
+
+for epoch in range(epochs):
+	model.train()
+	t_loss = 0
+	for step_train, data_train in enumerate(tqdm(train_loader)):
+		imgLR, imgHR = data_train
+		optimizer.zero_grad()
+		out = model(imgLR.to(device))
+		loss = criterion(out, imgHR.to(device))
+		t_loss += loss.item()
+		loss.backward()
+		optimizer.step()
+
+	print("===> Epoch {} Complete: Avg. Loss: {:.4f}".format(epoch, t_loss))
+	model.eval()
+	with torch.no_grad():
+		for step_test, data_test in enumerate(tqdm(valid_loader)):
+			imgLRt, imgHRt = data_test
+			result = model(imgLRt.to(device))
+			MSE = criterion(result, imgHRt.to(device))
+			psnr = 10 * log10(1 / MSE.item())
+			# sum_psnr += psnr
+		print("**Average PSNR: {} dB".format(psnr))
+		torch.save(model.state_dict(), f'logs/{round(psnr, 5)}.pkl')
+
+
+
+
